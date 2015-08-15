83e8c0e03366f618e76989269454a9abc7ae4b24
Nico Schmoigl <github@schmoigl-online.de>
2015-08-15 15:11:24 +0200

    Remove unnecessary TODOs (they are no stubs...)

--- src/main/java/com/github/eaglerainbow/gitservlet/LocalInternalServerException.java
+++ src/main/java/com/github/eaglerainbow/gitservlet/LocalInternalServerException.java
@@ -4,17 +4,14 @@ public class LocalInternalServerException extends Exception {
 
 	public LocalInternalServerException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public LocalInternalServerException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public LocalInternalServerException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 	
 }
